0ed42172800d454a7d7cae061e5d6e6913e1895e
IlyaPIV <petermoloch@mail.ru>
2022-08-14 11:26:44 +0300

    users/pass in DB

--- src/main/java/com/udemy/spring/security/configuration/MyConfig.java
+++ src/main/java/com/udemy/spring/security/configuration/MyConfig.java
@@ -1,11 +1,15 @@
 package com.udemy.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.udemy.spring.security")
@@ -19,6 +23,21 @@ public class MyConfig {
         internalResourceViewResolver.setSuffix(".jsp");
 
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/udemy_db?useSSL=false&serverTimezone=UTC");
+            dataSource.setUser("bestuser");
+            dataSource.setPassword("bestuser");
+
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
     }
 
 }
